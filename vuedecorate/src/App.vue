dde7755b87de3c1b77701e11ac9b09be3c6efe05
tsutsuku <tsutsuku@aliyun.com>
2018-08-29 21:11:56 +0800

    #180829
    init vue

---
+++ vuedecorate/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <div class="container">
+      <nav class="navbar navbar-dark bg-inverse navbar-fixed-top">
+        <router-link to="/" class="navbar-brand text-success">检测室</router-link>
+        <ul class="nav-item">
+          <router-link to="/home" class="nav-link"/>
+        </ul>
+      </nav>
+    </div>
+  </div>
+</template>
+
+<script>
+
+  import Siderbar from './components/Sidebar.vue'
+
+  export default {
+    name: 'App',
+    'sidebar': Siderbar
+  }
+
+</script>
+
+<style>
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+</style>
